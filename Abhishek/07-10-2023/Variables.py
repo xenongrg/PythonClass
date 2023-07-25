3b2fe998d2fa95c727cdb0f52ba219860bcfdc3b
Abhishek Rai <raiabyss@gmail.com>
2023-07-25 12:37:57 +0545

    math homework

--- Abhishek/07-10-2023/Variables.py
+++ Abhishek/07-10-2023/Variables.py
@@ -1,5 +1,8 @@
-alphabet = ["apple","ball","cat"]
-x, y, z = alphabet
+#If you have a collection of values in a list, tuple etc.
+#Python allows you to extract the values into variables.
+#This is called unpacking.
+alphabet = ["apple","ball","cat"] #this is a collection of values or tuple
+x, y, z = alphabet #unpacking is done here
 print(x)
 print(y)
 print(z)
