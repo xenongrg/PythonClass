efd89c368194750a2b8db865a228d33543a2f2fd
Abhishek Rai <raiabyss@gmail.com>
2023-07-25 12:32:59 +0545

    math homework

--- Abhishek/07-25-2023/Math.py
+++ Abhishek/07-25-2023/Math.py
@@ -1,10 +1,6 @@
 x = float(input ("Enter first number: "))
 y = float(input ("Enter second number: "))
-result1 = x + y
-result2 = x - y
-result3 = x * y
-result4 = x / y
-print(x,"+", y, "=",result1)
-print(x,"-", y, "=",result2)
-print(x,"*", y, "=",result3)
-print(x,"/", y, "=",result4)
+print(x,"+", y, "=",x + y)
+print(x,"-", y, "=",x - y)
+print(x,"*", y, "=",x * y)
+print(x,"/", y, "=",x / y)
