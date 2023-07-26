b9e34ac83582cb76b8ec4a9decbd31e6445f5448
Sahan <sahan.lama55@gmail.com>
2023-07-26 12:02:53 +0545

    Sahan Classwork 7-26-2023 : Sent two files

---
+++ Sahan/07-26-2023/GreaterThanThreeInput.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a>b and a>c:
+    print(a,"is greater than", b, "and", c)
+elif b>a and b>c:
+    print(b,"is greater than", a, "and", c)
+else:
+    print(c,"is greater than", a, "and", b)
