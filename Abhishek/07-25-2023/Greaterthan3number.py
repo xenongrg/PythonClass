9b5599db87e396f0aa8da581a770e7b5dc0263e4
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-07-26 11:58:45 +0545

    Conditional classwork

--- Abhishek/07-25-2023/Greaterthan3number.py
+++ Abhishek/07-25-2023/Greaterthan3number.py
@@ -1,9 +1,9 @@
-x = input("Enter 1st number: ")
-y = input("Enter 2nd number: ")
-z = input("Enter 3rd number: ")
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+z = int(input("Enter 3rd number: "))
 if x>y and x>z:
-    print(x + " is the greater one")
+    print(x," is the greater one")
 elif y>x and y>z:
-    print(y + " is the greater one")
+    print(y," is the greater one")
 else:
-    print(z + " is the greater one")
+    print(z," is the greater one")
