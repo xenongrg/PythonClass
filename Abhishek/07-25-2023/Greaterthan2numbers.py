afa36e1a8c950e514e99336ad0d1f8b313faea67
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-07-26 11:49:39 +0545

    Conditional classwork

---
+++ Abhishek/07-25-2023/Greaterthan2numbers.py
@@ -0,0 +1,6 @@
+x = input("Enter 1st number: ")
+y = input("Enter 2nd number: ")
+if x > y:
+    print(x + " is greater")
+else:
+    print(y + " is greater")
---
+++ Abhishek/07-25-2023/Greaterthan3number.py
@@ -0,0 +1,9 @@
+x = input("Enter 1st number: ")
+y = input("Enter 2nd number: ")
+z = input("Enter 3rd number: ")
+if x>y and x>z:
+    print(x + " is the greater one")
+elif y>x and y>z:
+    print(y + " is the greater one")
+else:
+    print(z + " is the greater one")
