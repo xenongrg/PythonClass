ccd74ef9e2e8cff0cc425a85db478d93f0137007
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-07-27 12:08:48 +0545

    Optimized

--- Abhishek/07-25-2023/Greaterthan2numbers.py
+++ Abhishek/07-25-2023/Greaterthan2numbers.py
@@ -1,6 +1,6 @@
-x = input("Enter 1st number: ")
-y = input("Enter 2nd number: ")
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
 if x > y:
-    print(x + " is greater")
+    print(x, " is greater")
 else:
-    print(y + " is greater")
+    print(y, " is greater")
--- Abhishek/07-25-2023/Greaterthan3number.py
+++ Abhishek/07-25-2023/Greaterthan3number.py
@@ -1,9 +1,13 @@
 x = int(input("Enter 1st number: "))
 y = int(input("Enter 2nd number: "))
 z = int(input("Enter 3rd number: "))
-if x>y and x>z:
-    print(x," is the greater one")
-elif y>x and y>z:
-    print(y," is the greater one")
+if x > y:
+    if x > z:
+        print(f"{x} is greater")
+    else:
+        print(f"{z} is greater")
 else:
-    print(z," is the greater one")
+    if y > z:
+        print(f"{y} is greater")
+    else:
+        print(f"{z} is greater")
