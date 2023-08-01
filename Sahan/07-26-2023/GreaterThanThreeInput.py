ab4839e10987948b3277bb1f52ec8f4b20c6919c
Sahan <sahan.lama55@gmail.com>
2023-08-01 11:35:55 +0545

    Sahan Homework 7-26-2023 Nested if

--- Sahan/07-26-2023/GreaterThanThreeInput.py
+++ Sahan/07-26-2023/GreaterThanThreeInput.py
@@ -1,6 +1,6 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
 if a > b:
     if a > c:
         print(f"{a} is greater than {b} and {c}")
