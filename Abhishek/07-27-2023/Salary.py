475474d8d90f00e76f74518e1bd9fda2b27096bb
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-08-01 11:49:37 +0545

    Optimized with less lines of code

--- Abhishek/07-27-2023/Salary.py
+++ Abhishek/07-27-2023/Salary.py
@@ -4,18 +4,14 @@ User2Salary = int(input("Enter Salary2: "))
 User2Name = input("Enter First name2: ")
 User3Salary = int(input("Enter Salary3: "))
 User3Name = input("Enter First name3: ")
+
 if User1Salary >= 100000:
-    x = User1Salary - (User1Salary * 0.1)
-    print(User1Name, "earns", x)
-else:
-    print(User1Name, "earns", User1Salary)
+    User1Salary = User1Salary - (User1Salary * 0.1)
 if User2Salary >= 100000:
-    y = User2Salary - (User2Salary * 0.1)
-    print(User2Name, "earns", y)
-else:
-    print(User2Name, "earns", User2Salary)
+    User2Salary = User2Salary - (User2Salary * 0.1)
 if User3Salary >= 100000:
-    z = User3Salary - (User3Salary * 0.1)
-    print(User3Name, "earns", z)
-else:
-    print(User3Name, "earns", User3Salary)
+    User3Salary = User3Salary - (User3Salary * 0.1)
+
+print(User1Name, "earns", User1Salary)
+print(User2Name, "earns", User2Salary)
+print(User3Name, "earns", User3Salary)
