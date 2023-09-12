1a4c7dffc85761dbd3ffa2a6391100b5682865b3
Nishant Gurung <xenongrg@gmail.com>
2023-09-12 11:19:52 +0545

    Delete Sahan/09-06-2023/PatternSecond.py

--- Sahan/09-06-2023/PatternSecond.py
+++
@@ -1,9 +0,0 @@
-num = 0
-for i in range(0, 5):
-    for j in range(0, 5 - i):
-        if num < 9:
-            print(num+1, end="      ")
-        else:
-            print(num+1, end="     ")
-        num = num + 1
-    print()
