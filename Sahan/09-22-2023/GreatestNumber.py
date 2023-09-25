39722ffff960241738314635e86a13eee4253831
Sahan <sahan.lama55@gmail.com>
2023-09-25 11:00:22 +0545

    Sahan Homework 09-22-2023

---
+++ Sahan/09-22-2023/GreatestNumber.py
@@ -0,0 +1,5 @@
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+arr = [x, y]
+arr.sort()
+print("The greatest number is:", arr[1])
---
+++ Sahan/09-22-2023/Swap.py
@@ -0,0 +1,7 @@
+x = "abcd"
+y = "efgh"
+arr = [x, y]
+arr.reverse()
+x = arr[0]
+y = arr[1]
+print(f'x = {x}\ny = {y}')
