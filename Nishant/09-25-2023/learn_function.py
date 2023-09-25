96f8fb329d74b46d7605b3833a65df103bb9dbbd
xenongrg <xenongrg@gmail.com>
2023-09-25 11:45:36 +0545

    learn_function

---
+++ Nishant/09-25-2023/learn_function.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    summation = a + b
+    return summation
+
+
+x = int(input("Enter number:"))
+y = int(input("Enter another number:"))
+addition = add(x, y)
+print(f'Addition = {addition}')
