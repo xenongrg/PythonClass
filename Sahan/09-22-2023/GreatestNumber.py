6ce6400b32c9f7c699581739b1bca40ff01d2b22
Sahan <sahan.lama55@gmail.com>
2023-09-25 21:52:49 +0545

    Sahan Homework 09-22-2023 Update

--- Sahan/09-22-2023/GreatestNumber.py
+++ Sahan/09-22-2023/GreatestNumber.py
@@ -3,5 +3,7 @@ y = int(input("Enter second number: "))
 
 if x - y != abs(x - y):
     print(f'Greatest number is {y}')
+elif y - x != abs(y - x):
+    print(f'Greatest number is {x}')
 else:
-    print(f'Greatest number is{x}')
+    print("Both numbers are equal.")
