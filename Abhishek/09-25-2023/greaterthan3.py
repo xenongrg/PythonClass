6f1caab9ac4c4c7d6b830befe2bad421865a371a
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-09-27 11:25:52 +0545

    greatest value without comparison operators

--- Abhishek/09-25-2023/greaterthan3.py
+++ Abhishek/09-25-2023/greaterthan3.py
@@ -3,26 +3,16 @@ def compare(a, b):
     return comparison
 
 
-def compare1(a, c):
-    comparison1 = a > c
-    return comparison1
-
-
-def compare2(b, c):
-    comparison2 = b > c
-    return comparison2
-
-
 x = int(input("Enter 1st number: "))
 y = int(input("Enter 2nd number: "))
 z = int(input("Enter 3rd number: "))
 if compare(x, y):
-    if compare1(x, z):
+    if compare(x, z):
         print(f"{x} = a is greater")
     else:
         print(f"{z} = c is greater")
 else:
-    if compare2(y, z):
+    if compare(y, z):
         print(f"{y} = b is greater")
     else:
         print(f"{z} = c is greater")
