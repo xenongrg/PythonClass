1ac6a93ba8e98dbb8bbc81bc6faf8855d53971ae
Sahan <sahan.lama55@gmail.com>
2023-09-27 11:23:00 +0545

    Sahan Homework 09-22-2023 Update

--- Sahan/09-22-2023/GreatestNumber.py
+++ Sahan/09-22-2023/GreatestNumber.py
@@ -1,9 +1,6 @@
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
-if x - y != abs(x - y):
-    print(f'Greatest number is {y}')
-elif y - x != abs(y - x):
-    print(f'Greatest number is {x}')
-else:
-    print("Both numbers are equal.")
+a = x + y
+b = abs(x - y)
+print(f'Greatest number is {(a+b)/2}')
