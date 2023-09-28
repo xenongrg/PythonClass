83fdee3dc09264011381e73631b0eff4eba61852
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-09-28 19:45:38 +0545

    Revert "Revert "too much dherai vo""
    This reverts commit 57bac6e2ee227bbfe7c8afeee8dd8806c6934f4e.

---
+++ Abhishek/09-28-2023/fibonacci.py
@@ -0,0 +1,24 @@
+def fibonacci(a, b, c):
+    for i in range(a):
+        print(b)
+        n = b + c
+        b = c
+        c = n
+    return a
+
+
+r = int(input("Enter nth number: "))
+x = 0
+y = 1
+fibonacci(r, x, y)
+
+
+# without function
+# r = int(input("Enter nth number: "))
+# x = 0
+# y = 1
+# for i in range(r):
+#     print(x)
+#     n = x + y
+#     x = y
+#     y = n
---
+++ Abhishek/09-28-2023/palindromestring.py
@@ -0,0 +1,7 @@
+x = input("Enter a string: ")
+y = x[::-1]
+# print(y)
+if x == y:
+    print("This is palindrome")
+else:
+    print("This is not palindrome")
