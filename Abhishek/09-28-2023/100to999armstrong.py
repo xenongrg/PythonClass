57bac6e2ee227bbfe7c8afeee8dd8806c6934f4e
kehsihba963 <abhishek.rai0000369@gmail.com>
2023-09-28 19:44:52 +0545

    Revert "too much dherai vo"
    This reverts commit 8fc0cde943de96b846db607e072217dd0b5094d9.

--- Abhishek/09-28-2023/100to999armstrong.py
+++
@@ -1,9 +0,0 @@
-for i in range(100, 1000):
-    x = i
-    armstrong = 0
-    while x > 0:
-        armstrong = armstrong + (x % 10)**3
-        x = x // 10
-
-    if i == armstrong:
-        print(armstrong, end=",")
--- Abhishek/09-28-2023/palindromestring.py
+++
@@ -1,7 +0,0 @@
-x = input("Enter a string: ")
-y = x[::-1]
-# print(y)
-if x == y:
-    print("This is palindrome")
-else:
-    print("This is not palindrome")
