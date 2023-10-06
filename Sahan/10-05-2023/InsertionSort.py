638c664c6bfac5ad322946294e6e67c556a976cc
Sahan <sahan.lama55@gmail.com>
2023-10-06 22:28:08 +0545

    Sahan Homework 10-05-2023 (InsertionSort Updated - SelectionSort Pending)

--- Sahan/10-05-2023/InsertionSort.py
+++ Sahan/10-05-2023/InsertionSort.py
@@ -1,12 +1,12 @@
-a = [2, 8, 5, 3, 9]
+a = [9, 8, 5, 3, 2]
 for j in range(4):
     if a[j + 1] < a[j]:
         temp = a[j + 1]
         a[j + 1] = a[j]
         a[j] = temp
         for i in range(j):
-            if a[j] < a[j - 1]:
-                temp2 = a[j]
-                a[j] = a[j - 1]
-                a[j - 1] = temp
+            if a[j - i] < a[j - i - 1]:
+                temp2 = a[j - i]
+                a[j - i] = a[j - i - 1]
+                a[j - i - 1] = temp
 print(a)
